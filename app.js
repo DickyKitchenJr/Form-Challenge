471f562b5f612b3ad74b978804930984d408546c
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-07-25 15:38:29 -0400

    enable submit button if conditions are met

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-function submitEnabled(){
-    document.getElementById("submit").removeAttribute("disabled");
-}; 
-
-function changeSubmit(){
-    let firstName = document.getElementById("first-name").value.length;
-    let lastName = document.getElementById("last-name").value.length;
-    if(firstName >= 1 && lastName >=1){
-        return submitEnabled();
-    };
+// remove disabled from the submit button
+function submitEnabled() {
+  document.getElementById("submit").removeAttribute("disabled");
 }
 
-const firstNameCheck = document.getElementById("first-name")
-const lastNameCheck = document.getElementById("last-name")
+let firstNameCheck = null;
+let lastNameCheck = null;
 
-firstNameCheck.onkeydown = changeSubmit;
-lastNameCheck.onkeydown = changeSubmit;
+// verify first name box has a user input, if so update firstNameCheck
+function firstInput() {
+  let firstName = document.getElementById("first-name").value.length;
+  if (firstName != 0) {
+    firstNameCheck = document.getElementById("first-name");
+  }
+  return firstNameCheck;
+}
+
+// verify last name box has a user input, if so update lastNameCheck
+function lastInput() {
+  let lastName = document.getElementById("last-name").value.length;
+  if (lastName != 0) {
+    lastNameCheck = document.getElementById("last-name");
+  }
+  return lastNameCheck;
+}
+
+// enable submit button if first name and last name have at least 1 character input in each
+function activate() {
+    firstInput();
+    lastInput();
+  if (firstNameCheck != null && lastNameCheck != null) {
+    submitEnabled();
+  } else {
+    return;
+  }
+}
